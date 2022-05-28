9118ca0291bfd202e98a1d5ab1f63cfede5c2fd3
autobele <autobele@infoway-pi.com.br>
2022-05-27 22:54:51 -0300

    add create users and show todos

--- src/index.js
+++ src/index.js
@@ -1,25 +1,49 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const userAccountAlreadyExist = users.some(user => user.username === username)
+
+   if(!userAccountAlreadyExist) {
+     return response.status(400).json({ error: 'User account not exist'});
+   }
+   
+   request.username = username
+
+   next()
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const { name, username } = request.body;
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user)
+  return response.json(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { username } = request
+  const user = users.find(user => user.username === username)
+
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
